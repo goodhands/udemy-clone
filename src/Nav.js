8e224b15c5c53287982cabf6bfb684d351a0c959
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-12-01 15:00:46 +0100

    add useHover for cart cart

--- src/Nav.js
+++ src/Nav.js
@@ -3,9 +3,14 @@ import useHover from './hooks/useHover';
 import Button from './components/button/Button';
 import Categories from './components/menu/Categories';
 
+const cartIcon = <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+
 const Nav = () => {
     const [teach, TeachMenu, setTeach] = useHover("Teach on Udemy", false);
     const [business, BusinessMenu, setBusinessMenu] = useHover("Udemy for Business", false);
+    const [cart, Cart, setCart] = useHover(cartIcon, false);
 
     return (
         <nav className="bg-white border-b-2 border-gray-200 flex flex-row h-16 items-center space-x-4 px-5 shadow-sm">
@@ -39,16 +44,17 @@ const Nav = () => {
                     </div>
                 </TeachMenu>
 
-                <a href="" className="nav-menu">
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                </a>
+                <Cart>
+                    <div className="bg-white border flex flex-col items-center justify-center p-3 space-y-4 w-72">
+                        <p>Your cart is empty.</p>
+                        <a href="#" className="text-primary font-bold">Keep shopping</a>
+                    </div>
+                </Cart>
             </div>
 
             <div className="buttons flex justify-end space-x-3 w-2/12">
                 <a href="" className="btn default w-24 h-10">Log in</a>
-                <a href="" className="btn primary w-24 h-10">Sign up</a>
+                <a href="" className="btn primary w-24 h-10 hover:bg-neutral">Sign up</a>
             </div>
         </nav>
     )
