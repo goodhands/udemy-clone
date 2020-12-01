0d03fd4ed8f678ab5ac492bc568311e601d89382
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-12-01 16:06:50 +0100

    add home page teaser

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ const App = () => {
     return (
         <div>
             <Nav/>
+            <div className="home-bg-teaser w-full p-20">
+                <div className="bg-white border shadow w-5/12 p-10">
+                    <h2 className="text-4xl text-neutral font-bold">Cyber deals.</h2>
+                    <h2 className="text-4xl text-neutral font-bold">Real skills.</h2>
+                    <p className="text-base mt-2">
+                        Courses as low as $9.99 through Dec. 4. Plus, get free access to a <a href="#">live webinar!</a>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
