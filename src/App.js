3ff8ee04ea66cff288078899cde7ec3e709f3bf7
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-12-02 21:34:31 +0100

    add homepage header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import axios from './axios';
 import Nav from './Nav';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <div>
             <Nav/>
-            <div className="home-bg-teaser w-full p-20">
-                <div className="bg-white border shadow w-5/12 p-10">
-                    <h2 className="text-4xl text-neutral font-bold">Cyber deals.</h2>
-                    <h2 className="text-4xl text-neutral font-bold">Real skills.</h2>
-                    <p className="text-base mt-2">
-                        Courses as low as $9.99 through Dec. 4. Plus, get free access to a <a href="#">live webinar!</a>
-                    </p>
-                </div>
-            </div>
+            <Home />
         </div>
     )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section>
+            <div className="home-bg-teaser w-full p-10">
+                <div className="bg-white border shadow w-5/12 p-10">
+                    <h2 className="text-4xl text-neutral font-bold">Cyber deals.</h2>
+                    <h2 className="text-4xl text-neutral font-bold">Real skills.</h2>
+                    <p className="text-xl mt-2 text-neutral">
+                        Courses as low as $9.99 through Dec. 4. Plus, get free access to a <a href="#" className="text-primary underline">live webinar!</a>
+                    </p>
+                </div>
+            </div>
+            <div className="my-14 px-5 catalog">
+                <h2 className="text-2xl text-neutral font-bold">
+                    The world's largest selection of courses
+                </h2>
+                <p className="text-neutral text-base">
+                    Choose from 130,000 online video courses with new additions published every month
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
